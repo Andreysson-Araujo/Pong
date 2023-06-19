c95c955740f880fd2749228ac296c8d3327d4789
Andreysson-Araujo <andreyssonaraujo@gmail.com>
2023-06-18 21:09:44 -0500

    jogo Funcional

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
+  let scorePlayer = 0;
+  let scoreEnemy = 0;
+  const colors = ["#FD0000", "#0018FD" , "#00FD0B" , "#CF00FD" , "#FD008A", "#000000"]
   const canvas = document.querySelector("#pong-canvas");
-  const context = canvas.getContext("2d")
-
+  const context = canvas.getContext("2d");
 
   const ball = {
     x: canvas.width / 2,
@@ -10,9 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
     speed: 4,
     dx: 4,
     dy: 4
-  }
-
-
+  };
 
   const paddle = {
     width: 10,
@@ -21,91 +21,126 @@ document.addEventListener("DOMContentLoaded", () => {
     y: canvas.height / 2 - 40,
     dy: 8,
     upPressed: false,
-    downPressed: false,
+    downPressed: false
   };
 
-  const clonePaddle = {
+  const enemyPaddle = {
     width: 10,
-    height: 500,
-    x: 0,
+    height: 80,
+    x: canvas.width - 10,
     y: canvas.height / 2 - 40,
-    dy: 8,
-    upPressed: false,
-    downPressed: false,
+    dy: 3
   };
 
-
-  //DESENHA BOLA
-
-  function createBall(){
-      context.beginPath();
-      context.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
-      context.fillStyle = "#fff"
-      context.fill();
-      context.closePath()
-  }
-
-
-  function createPaddle() {
+  function createBall() {
     context.beginPath();
-    context.rect(paddle.x, paddle.y, paddle.width, paddle.height);
-    context.fillStyle = "#fff"
-    context.fill();
-    context.closePath()
-  }
-
-  function createOpponentPaddle() {
-    const opponentPaddleX = canvas.width - clonePaddle.width - clonePaddle.x; // Posição X do clone da raquete
-    const opponentPaddley = clonePaddle.y = 0
-    context.beginPath();
-    context.rect(opponentPaddleX, clonePaddle.y, clonePaddle.width, clonePaddle.height);
+    context.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
     context.fillStyle = "#fff";
     context.fill();
     context.closePath();
   }
-  
 
+  function createPaddle() {
+    context.beginPath();
+    context.rect(paddle.x, paddle.y, paddle.width, paddle.height);
+    context.fillStyle = "#fff";
+    context.fill();
+    context.closePath();
+  }
+
+  function moveEnemyPaddle() {
+    const enemyPaddleCenter = enemyPaddle.y + enemyPaddle.height / 2;
+
+    if (ball.y < enemyPaddleCenter) {
+      enemyPaddle.y -= enemyPaddle.dy;
+    } else if (ball.y > enemyPaddleCenter) {
+      enemyPaddle.y += enemyPaddle.dy;
+    }
+
+    if (enemyPaddle.y < 0) {
+      enemyPaddle.y = 0;
+    } else if (enemyPaddle.y + enemyPaddle.height >= canvas.height) {
+      enemyPaddle.y = canvas.height - enemyPaddle.height;
+    }
+  }
+
+  function createEnemyPaddle() {
+    context.beginPath();
+    context.rect(enemyPaddle.x, enemyPaddle.y, enemyPaddle.width, enemyPaddle.height);
+    context.fillStyle = "#fff";
+    context.fill();
+    context.closePath();
+  }
+
+  function colorize(){
+    const randomColor = Math.floor(Math.random() * colors.length)
+    document.body.style.background = colors[randomColor]
+  }
 
   function update() {
-  if (paddle.upPressed && paddle.y > 0) {
-    paddle.y -= paddle.dy;
-  } else if (paddle.downPressed && paddle.y + paddle.height < canvas.height) {
-    paddle.y += paddle.dy;
+    if (paddle.upPressed && paddle.y > 0) {
+      paddle.y -= paddle.dy;
+    } else if (paddle.downPressed && paddle.y + paddle.height < canvas.height) {
+      paddle.y += paddle.dy;
+    }
+
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+
+    if (ball.y + ball.radius > canvas.height || ball.y - ball.radius < 0) {
+      ball.dy *= -1;
+    }
+
+    if (
+      ball.x - ball.radius < paddle.x + paddle.width &&
+      ball.y + ball.radius > paddle.y &&
+      ball.y - ball.radius < paddle.y + paddle.height
+    ) {
+      ball.dx *= -1;
+    }
+
+    if (
+      ball.x + ball.radius > canvas.width - paddle.width &&
+      ball.y + ball.radius >= paddle.y &&
+      ball.y - ball.radius <= paddle.y + paddle.height
+    ) {
+      ball.dx *= -1;
+    }
+
+    if (
+      ball.x + ball.radius > canvas.width - enemyPaddle.width &&
+      ball.y + ball.radius >= enemyPaddle.y &&
+      ball.y - ball.radius <= enemyPaddle.y + enemyPaddle.height
+    ) {
+      ball.dx *= -1;
+    }
+
+    if (ball.x + ball.radius < 0) {
+      scoreEnemy++;
+      document.getElementById("score-enemy").textContent = scoreEnemy;
+      //colorize()
+      resetBall();
+    } else if (ball.x - ball.radius > canvas.width) {
+      scorePlayer++;
+      document.getElementById("score-player").textContent = scorePlayer;
+      //colorize()
+      resetBall();
+    }
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    createBall();
+    createPaddle();
+    createEnemyPaddle();
+    moveEnemyPaddle();
+    requestAnimationFrame(update);
   }
 
-  ball.x += ball.dx;
-  ball.y += ball.dy;
-
-  if (ball.y + ball.radius > canvas.height || ball.y - ball.radius < 0) {
+  function resetBall() {
+    ball.x = canvas.width / 2;
+    ball.y = canvas.height / 2;
+    ball.dx *= -1;
     ball.dy *= -1;
   }
-
-  if (
-    ball.x - ball.radius < paddle.x + paddle.width &&
-    ball.y + ball.radius > paddle.y &&
-    ball.y - ball.radius < paddle.y + paddle.height
-  ) {
-    ball.dx *= -1;
-  }
-
-  if (
-    ball.x + ball.radius > canvas.width - paddle.width && // Verificar colisão horizontal
-    ball.y + ball.radius >= paddle.y && // Verificar colisão superior
-    ball.y - ball.radius <= paddle.y + paddle.height // Verificar colisão inferior
-  ) {
-    ball.dx *= -1; // Inverter direção horizontal da bola
-  }
-
-  context.clearRect(0, 0, canvas.width, canvas.height);
-
-  createBall();
-  createPaddle();
-
-  //clone 
-  createOpponentPaddle()
-  
-  requestAnimationFrame(update);
-}
 
   document.addEventListener("keydown", (event) => {
     if (event.key === "ArrowUp" || event.key === "Up") {
@@ -123,7 +158,5 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-
-  update()
+  update();
 });
-
